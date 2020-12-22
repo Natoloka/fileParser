dcc3c4843715e9b4c9b4b1c6f03998d710e9dd48
Natoloka <natoloka@ukr.net>
2020-12-22 09:01:36 +0200

    Added array with names, Todo: complete the matcher

--- src/main/java/ParsingHelper.java
+++ src/main/java/ParsingHelper.java
@@ -4,13 +4,28 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class ParsingHelper {
     private static final int BUFFER_SIZE = 4096;
+
     public void parseFile(String fileURL, String saveDir) throws IOException {
         URL url = new URL(fileURL);
+        String listOfNames = "James,\n" +
+                "                John,Robert,Michael,William,David,Richard,Charles, Joseph, Thomas,Christopher,\n" +
+                "                Daniel,Paul,Mark,D onald,George,Kenneth,Steven, Edward,\n" +
+                "                Brian,Ronald,Anthony,Kevin,Jason,Matthew, Gary, Timothy,Jose,Larr\n" +
+                "                y,Jeffrey,Frank,Scott,Eric,Stephen,Andrew,Raymond,Gregory,Joshua,Jerry,Dennis, Walter,\n" +
+                "                Patrick, Peter, Har old,Douglas, Henry, Carl,Arthur,Ryan,Roger";
+
+        String[] namesArray = listOfNames.replaceAll("(\\s|\\n)", "").split(",");
+        System.out.println(namesArray);
+
         HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
         int responseCode = httpConn.getResponseCode();
 
@@ -34,10 +49,10 @@ public class ParsingHelper {
                         fileURL.length());
             }
 
-            System.out.println("Content-Type = " + contentType);
-            System.out.println("Content-Disposition = " + disposition);
-            System.out.println("Content-Length = " + contentLength);
-            System.out.println("fileName = " + fileName);
+//            System.out.println("Content-Type = " + contentType);
+//            System.out.println("Content-Disposition = " + disposition);
+//            System.out.println("Content-Length = " + contentLength);
+//            System.out.println("fileName = " + fileName);
 
             // opens input stream from the HTTP connection
             InputStream inputStream = httpConn.getInputStream();
@@ -52,14 +67,37 @@ public class ParsingHelper {
                 outputStream.write(buffer, 0, bytesRead);
             }
 
-            outputStream.close();
-            inputStream.close();
 
-            System.out.println("File downloaded");
-        } else {
-            System.out.println("No file to download. Server replied HTTP code: " + responseCode);
+//            TODO: complete matcher
+//            Pattern findWordPattern = Pattern.compile("his");
+//            for (int i = 0; i < 1000; i++) {
+//                Matcher matcher = findWordPattern.matcher(myWholeFileInAString);
+//                while (matcher.find()) {
+//                    int offsetStart = matcher.start();
+//                    int offsetEnd = matcher.end();
+//
+//                }
+//
+//                FileReader fileReader = new FileReader(fileName);
+//                try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+//                    String line;
+//                    while ((line = bufferedReader.readLine()) != null) {
+//                        if (line.contains(namesArray[0])) {
+//                            System.out.println(line.offsetByCodePoints());
+//                        }
+//
+//                    }
+//                }
+//            ----------
+
+                outputStream.close();
+                inputStream.close();
+
+                System.out.println("File downloaded");
+            } else{
+                System.out.println("No file to download. Server replied HTTP code: " + responseCode);
+            }
+            httpConn.disconnect();
+
         }
-        httpConn.disconnect();
-
     }
-}
